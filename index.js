0d0b998aaf7de6c1a53eba6743c89c52545a5256
Manarul22 <manarul0022@gmail.com>
2022-05-26 06:22:59 +0700

    menambahkan data user

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+const firstUser = {
+    "name": "Andi",
+    "age": 24
+};
+const secondUser = {
+    "name":"Sutomo",
+    "age": 25
+};
+
+const desc = "data user : \n";
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
-const users = [];
+const users = [desc, firstUser, secondUser];
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
