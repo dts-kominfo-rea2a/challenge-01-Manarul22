8953375f2f30934ca5258b4d2c7e5c5725987105
Manarul22 <manarul0022@gmail.com>
2022-05-26 08:13:00 +0700

    update data user

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
 const firstUser = {
-  name: "Andi",
-  gender: "Male",
+  name: "Meni",
+  gender: "Female",
   age: 24,
   email: "andi@gmail.com",
   favoriteColor: ["purple, black, blue"],
@@ -28,31 +28,31 @@ const firstUser = {
   favoriteRestaurant: ["Baso Pengkolan", "Baso Aci", "Mie Ayam Tawon"],
 };
 const secondUser = {
-    name: "Sutomo",
-    gender: "Male",
-    age: 24,
-    email: "sutomo@gmail.com",
-    favoriteColor: ["magenta, ocean, lightblue"],
-    isHavePet: true,
-    education: [
-      {
-        name: "SD 01",
-        city: "Jakarta",
-        graduate: 2016,
-      },
-      {
-        name: "SMP 02",
-        city: "Jakarta",
-        graduate: 2019,
-      },
-      {
-        name: "SMA 03",
-        city: "Tangerang",
-        graduate: 2022,
-      },
-    ],
-    favoriteRestaurant: ["Baso Ceu Onah", "Baso EMas", "Mie Ayam Kangkung"],
-  };
+  name: "Sutomo",
+  gender: "Male",
+  age: 24,
+  email: "sutomo@gmail.com",
+  favoriteColor: ["magenta, ocean, lightblue"],
+  isHavePet: true,
+  education: [
+    {
+      name: "SD 01",
+      city: "Jakarta",
+      graduate: 2016,
+    },
+    {
+      name: "SMP 02",
+      city: "Jakarta",
+      graduate: 2019,
+    },
+    {
+      name: "SMA 03",
+      city: "Tangerang",
+      graduate: 2022,
+    },
+  ],
+  favoriteRestaurant: ["Baso Ceu Onah", "Baso EMas", "Mie Ayam Kangkung"],
+};
 
 const desc = "Data User :";
 
